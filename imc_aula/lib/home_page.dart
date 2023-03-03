2ed8229c50e88cc9130a748c1e267ceba4a0b777
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-03-03 18:44:59 -0300

    Initial commit

---
+++ imc_aula/lib/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:imc_aula/setState/imc_setstate.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void _goToPage(BuildContext context, Widget page) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => page,),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => _goToPage(context, const ImcSetstate()),
+              child: const Text('SetState'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('ValueNotifier'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('ChangeNotifier'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Bloc Pattern (Streams)'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
